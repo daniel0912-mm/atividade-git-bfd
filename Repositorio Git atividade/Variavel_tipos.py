2411527683c0c270b7c29484d6a33004b36b84ae
Daniel <daniel0912.com@gmail.com>
2025-09-14 15:34:29 -0300

    controle condicional

---
+++ Repositorio Git atividade/Variavel_tipos.py
@@ -0,0 +1,13 @@
+# Variaveis
+
+nome = "João"  # String
+idade = 25      # Inteiro
+altura = 1.75   # Float
+is_student = True  # Booleano
+print("Nome:", nome) # Imprime o valor da variavel nome e retorna uma string
+print("Idade:", idade) # Imprime o valor da variavel idade e retorna um numero inteiro
+print("Altura:", altura) # Imprime o valor da variavel altura e retorna um numero float
+print("É estudante:", is_student) # Imprime o valor da variavel is_student e retorna um booleano
+
+print(float(idade))  # Converte inteiro para float e imprime um numero com casas decimais truncadas
+print(int(altura))  # Converte float para inteiro e imprime um numero sem casas decimais
