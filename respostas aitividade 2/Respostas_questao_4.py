cf5f1a1825cbf7ba27f69020ddb9346f3a2fd279
Daniel <daniel0912.com@gmail.com>
2025-09-14 19:58:31 -0300

    remoção atividades anteriores tarefa 1

--- respostas aitividade 2/Respostas_questao_4.py
+++
@@ -1,5 +0,0 @@
-#Conversor de temperatura
-
-celsius = float(input("Digite a temperatura em Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print(f"A temperatura em Fahrenheit é: {fahrenheit:.2f}°F")
